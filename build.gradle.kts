7fd9fd1c47fe1d50124b6b0ac531194df5e096e8
srudnytsky <s.rudnytsky@student.neoversity.com.ua>
2026-05-25 06:13:51 +0000

    Update dependecies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("com.h2database:h2:2.2.224")
+    implementation("org.flywaydb:flyway-core:10.10.0")
 }
 
 tasks.test {
